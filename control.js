282b10b24940dad4489781056fac33b577ee24da
Arda Yasar <50085179+ardayasar@users.noreply.github.com>
2024-09-22 09:52:17 +0300

    Change of files

--- control.js
+++ control.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 
-console.log(JSON.parse(fs.readFileSync(__dirname + '/database/anime_informations.json'))['animes']['1']);
+console.log(JSON.parse(fs.readFileSync(__dirname + '/database/anime_informations.json'))['animes']['1']['tags']['tag']);
 // console.log(Object.keys(JSON.parse(fs.readFileSync(__dirname + '/database/anime-titles.json'))));
--- importer.js
+++ importer.js
@@ -16,15 +16,21 @@ const total_animes = Object.keys(local_information_database['animes']);
 
 (async() => {
 
-    console.log(local_information_database['animes'].length);
+    // console.log(local_information_database['animes'].length);
 
-    for (let i = 0; i < total_animes.length; i++) {
+    for (let i = 0; i < total_animes.length; i++) { // total_animes.length
         id = total_animes[i];
         let anime = await get_information(id);
         
         console.log(colors.green + 'Title: ' + colors.reset + colors.blue + anime.data.title + colors.reset);
-        console.log(colors.green + 'Description: ' + colors.reset + colors.blue + anime.data.description + colors.reset);
+        if(anime.data.description.toLowerCase().includes('source:') || anime.data.description.toLowerCase().includes('note:')){
+            console.log(colors.green + 'Description: ' + colors.reset + colors.yellow + anime.data.description + colors.reset);
+        }else{
+            console.log(colors.green + 'Description: ' + colors.reset + colors.blue + anime.data.description + colors.reset);
+        }
         console.log(colors.green + 'Picture: ' + colors.reset + colors.blue + anime.data.picture_jpg + colors.reset);
+        console.log(colors.green + 'Rating: ' + colors.reset + colors.blue + anime.data.ratings + colors.reset);
+        console.log(colors.green + 'Tags: ' + colors.reset + colors.blue + anime.data.tags.join(', ') + colors.reset);
         console.log('-----------------------------------');
     }
 
@@ -52,11 +58,23 @@ function get_information(id) {
             console.log(colors.yellow + 'Title not found! Using the first title found...' + colors.reset);
         }
 
+        var anime_tags = [];
+        for(let i = 0; i < anime['tags']['tag'].length; i++) {
+            if(anime['tags']['tag'][i]['$']['infobox'] == 'true') {
+                let name = anime['tags']['tag'][i]['name'];
+
+                name = name.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+                anime_tags.push(name);
+            }
+        }
+
         var description = anime['description'];
         description = description.replace(/http[^\s]+\s*\[|\]/g, '');
 
         anime_data['description'] = description;
         anime_data['picture_jpg'] = anime['picture'];
+        anime_data['ratings'] = (parseFloat(anime['ratings']['temporary']['_'])/2).toFixed(1);
+        anime_data['tags'] = anime_tags;
 
         resolve({status: true, data: anime_data});
     });
